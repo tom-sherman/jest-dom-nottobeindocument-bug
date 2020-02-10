e50fdc6f0824566ace8fe2213239d00ce2b6f8ba
Tom Sherman <the.tomsherman@gmail.com>
2020-02-10 10:53:59 +0000

    Add failing test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = getByText(/definitely not in the document/i);
+  expect(linkElement).not.toBeInTheDocument();
 });
