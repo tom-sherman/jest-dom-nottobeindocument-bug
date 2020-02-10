eca61346d83bec2a1d0212b916bc9a8852dcf6e9
Tom Sherman <the.tomsherman@gmail.com>
2020-02-10 11:03:09 +0000

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/definitely not in the document/i);
-  expect(linkElement).not.toBeInTheDocument();
+  expect(() => getByText(/definitely not in the document/i)).toThrow()
+
 });
